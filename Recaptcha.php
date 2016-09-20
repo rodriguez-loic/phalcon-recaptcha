b9f4b947a7b151e42e31082bf36f1e37a358e4f0
RODRIGUEZ Loïc <contact@rodriguez-loic.me>
2016-09-20 13:56:41 +0100

    Update Recaptcha.php

--- Recaptcha.php
+++ Recaptcha.php
@@ -6,31 +6,10 @@
  *    - Get a reCAPTCHA API Key
  *          https://www.google.com/recaptcha/admin/create
  *
- * Copyright (c) 2007 reCAPTCHA -- http://recaptcha.net
- * AUTHORS:
- *   Mike Crawford
- *   Ben Maurer
- *   Pavlo Sadovyi (made this wrapper for Phalcon Framework)
- * CONTRIBUTORS:
- *   Loïc Rodriguez (adapt and simplify code to work with ReCAPTCHA v2 and Phalcon 3)
+ * This library is highly inspired from https://github.com/pavlosadovyi/phalcon-recaptcha
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * AUTHORS::
+ *   Loïc Rodriguez
  */
 class Recaptcha extends \Phalcon\DI\Injectable
 {
@@ -40,9 +19,6 @@ class Recaptcha extends \Phalcon\DI\Injectable
      */
     const API_PUBLIC_KEY = 's3cr3t';
     const API_PRIVATE_KEY = 'pr1v4t3-s3cr3t';
-
-    public static $error = 'incorrect-captcha-sol';
-    public static $is_valid = false;
 
     /**
      * Gets the challenge HTML (javascript and non-javascript version).
@@ -87,68 +63,43 @@ class Recaptcha extends \Phalcon\DI\Injectable
      * @param array $extra_params An array of extra variables to post to the server
      * @return boolean $this->is_valid property
      */
-    public static function check($response, $extra_params = array())
+    public static function check($gResponse)
     {
         // Discard spam submissions
-        if (!$challenge or !$response) {
-            return self::$is_valid;
+        if (!$gResponse) {
+            return false;
         }
 
         // Get response from API
-        $response = self::httpPost('www.google.com', "/recaptcha/api/verify", array(
-            'privatekey' => self::API_PRIVATE_KEY,
-            'remoteip' => $_SERVER['REMOTE_ADDR'],
-            'response' => $response
-        ) + $extra_params);
-        $answers = explode("\n", $response[1]);
+        $response = self::httpPost($gResponse);
 
-        if (trim($answers[0]) == 'true') self::$is_valid = true;
-        else self::$error = $answers[1];
-        return self::$is_valid;
+        return (boolean) $response->success;
     }
 
     /**
      * Submits an HTTP POST to a reCAPTCHA server
      *
-     * @param string $host
-     * @param string $path
-     * @param array $data
-     * @param int port
-     * @return array response
+     * @param array $gResponse
+     * @return array $response
      */
-    private static function httpPost($host, $path, $data, $port = 80)
+    private static function httpPost($gResponse)
     {
-        $req = self::qsEncode($data);
-        $http_request  = "POST $path HTTP/1.0\r\n";
-        $http_request .= "Host: $host\r\n";
-        $http_request .= "Content-Type: application/x-www-form-urlencoded;\r\n";
-        $http_request .= "Content-Length: ".strlen($req)."\r\n";
-        $http_request .= "User-Agent: reCAPTCHA/PHP\r\n";
-        $http_request .= "\r\n";
-        $http_request .= $req;
-        $response = '';
-        if (!($fs = @fsockopen($host, $port, $errno, $errstr, 10))) {
-            die('Could not open socket');
-        }
-        fwrite($fs, $http_request);
-        while (!feof($fs)) $response .= fgets($fs, 1160); // One TCP-IP packet
-        fclose($fs);
-        $response = explode("\r\n\r\n", $response, 2);
-        return $response;
-    }
-    /**
-     * Encodes the given data into a query string format
-     *
-     * @param array $data Array of string elements to be encoded
-     * @return string $req Encoded request
-     */
-    private static function qsEncode($data)
-    {
-        $req = '';
-        foreach ($data as $key => $value)
-            $req .= $key.'='.urlencode(stripslashes($value)).'&';
-        // Cut the last '&'
-        $req = substr($req, 0, strlen($req) - 1);
-        return $req;
+        $data = array(
+            'secret' => self::API_PRIVATE_KEY,
+            'response' => $gResponse
+        );
+
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+
+        $context = stream_context_create($options);
+        $result = file_get_contents("https://www.google.com/recaptcha/api/siteverify", false, $context);
+
+        return json_decode($result);
     }
 }
